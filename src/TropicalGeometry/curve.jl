1c365b1f1edafb2f87a35126bb040964e580cbcc
Lars Kastner <lars.kastner@posteo.de>
2022-02-28 11:05:01 +0100

    TropicalGeometry: Basics for divisors on tropical curves and chip firing

--- src/TropicalGeometry/curve.jl
+++ src/TropicalGeometry/curve.jl
@@ -19,8 +19,10 @@
 @attributes mutable struct TropicalCurve{M,EMB} <: TropicalVarietySupertype{M,EMB}
     polyhedralComplex::PolyhedralComplex
     function TropicalCurve{M,EMB}(Sigma::PolyhedralComplex) where {M,EMB}
-        if dim(Sigma)!=1
-            error("TropicalCurve: input polyhedral complex not one-dimensional")
+        if EMB
+            if dim(Sigma)!=1
+                error("TropicalCurve: input polyhedral complex not one-dimensional")
+            end
         end
         return new{M,EMB}(Sigma)
     end
@@ -58,6 +60,92 @@ function TropicalCurve{M, EMB}(Vertices::Union{SubObjectIterator{<:RayVector}, O
         return #...
     end
 end
+
+
+function TropicalCurve{M}(graph::IncidenceMatrix) where {M}
+    # Columns correspond to nodes
+    # Rows correpons to edges
+    empty = PolyhedralComplex(Polymake.fan.PolyhedralComplex())
+    result = TropicalCurve{M, false}(empty)
+    set_attribute!(result, :graph, graph)
+    return result
+end
+
+function graph(tc::TropicalCurve{M, EMB}) where {M, EMB}
+    if !has_attribute(tc, :graph)
+        throw(ArgumentError("No graph attached"))
+    end
+    return get_attribute(tc, :graph)
+end
+
+function n_nodes(tc::TropicalCurve{M, EMB}) where {M, EMB}
+    G = graph(tc)
+    return Polymake.ncols(G)
+end
+
+
+function Base.show(io::IO, tc::TropicalCurve{M, EMB}) where {M, EMB}
+    if EMB
+        print(io, "A tropical curve")
+    else
+        print(io, "An abstract tropical curve")
+    end
+end
+
+
+struct DivisorOnTropicalCurve{M, EMB}
+    base_curve::TropicalCurve{M, EMB}
+    coefficients::Vector{Int}
+    function DivisorOnTropicalCurve(tc::TropicalCurve{M, EMB}, coeffs::Vector{Int}) where {M,EMB}
+        if EMB
+            error("Not implemented yet")
+        else
+            if n_nodes(tc) != length(coeffs)
+                throw(ArgumentError("Wrong number coefficients"))
+            end
+            return new{M, EMB}(tc, coeffs)
+        end
+    end
+end
+
+base_curve(dtc::DivisorOnTropicalCurve{M, EMB}) where {M, EMB} = dtc.base_curve
+
+###
+# 3.Basic properties
+#
+coefficients(dtc::DivisorOnTropicalCurve{M, EMB}) where {M, EMB} = dtc.coefficients
+
+degree(dtc::DivisorOnTropicalCurve{M, EMB}) where {M, EMB} = sum(coefficients(dtc))
+
+is_effective(dtc::DivisorOnTropicalCurve{M, EMB}) where {M, EMB} = all(e -> e>=0, coefficients(dtc))
+
+function chip_firing_move(dtc::DivisorOnTropicalCurve{M, EMB}, position::Int) where {M, EMB}
+    G = graph(base_curve(dtc))
+    newcoeffs = Vector{Int}(coefficients(dtc))
+    for i in 1:Polymake.nrows(G)
+        row = Polymake.row(G, i)
+        if position in row
+            newcoeffs[position] -= 1
+            for i in row
+                if i != position
+                    newcoeffs[i] += 1
+                end
+            end
+        end
+    end
+    return DivisorOnTropicalCurve(base_curve(dtc), newcoeffs)
+end
+
+
+export DivisorOnTropicalCurve,
+    n_nodes,
+    degree,
+    coefficients,
+    is_effective,
+    graph,
+    base_curve,
+    chip_firing_move
+
 
 
 
